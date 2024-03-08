77da0032096ac18d2aef019d0cf87f1373689762
Vatsal Unadkat <vatsal@gatech.edu>
2024-03-08 16:01:21 +0530

    Update homepage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-        <header className="App-header">
-            <h1>GitHub Pages - Hello World</h1>
-        </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>GitHub Pages - Hello World!</h1>
+            </header>
+        </div>
+    );
 }
 
 export default App;
