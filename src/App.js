158fb8651bc350b03dd5562aeff69da29b29a098
Vatsal Unadkat <vatsal@gatech.edu>
2024-03-08 14:24:26 +0530

    Hello World Init - GH Pages

--- src/App.js
+++ src/App.js
@@ -4,12 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          GitHub Pages - Hello World
-        </p>
-      </header>
+        <header className="App-header">
+            <h1>GitHub Pages - Hello World</h1>
+        </header>
     </div>
   );
 }
